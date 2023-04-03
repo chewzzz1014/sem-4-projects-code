6b29114a0fc8b7061ade4d425e1559cf17d70e2c
chewzzz <chewziqing@gmail.com>
2023-04-03 16:59:26 +0800

    Add caeser_cipher.py

---
+++ CSC4502_Crypto/caeser_cipher.py
@@ -0,0 +1,11 @@
+# AUTHOR: Chew Zi Qing
+
+# textbook 3.32
+# question Write a program that can encrypt and decrypt using the general Caesar cipher, also known as an additive cipher.
+# key as input
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
